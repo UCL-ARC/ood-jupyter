b370bc7772386ab999ff5cf72d45ce82e87fafe0
Ian Kirker <ian.kirker@gmail.com>
2024-10-11 13:57:20 +0100

    Extends startup timeout from 60s to 5m

--- template/after.sh
+++ template/after.sh
@@ -1,7 +1,11 @@
 # Wait for the Jupyter Lab server to start
+
+# We had to extend this because it was taking Jupyter too long to start off Lustre
+timeout_seconds=300
+
 echo "Waiting for Jupyter Lab server to open port ${port}..."
 echo "TIMING - Starting wait at: $(date)"
-if wait_until_port_used "${host}:${port}" 60; then
+if wait_until_port_used "${host}:${port}" "$timeout_seconds"; then
   echo "Discovered Jupyter Lab server listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
 else
